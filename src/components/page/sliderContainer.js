2b476cef8d26c5f28cbc0172c9ca3bc232600901
Geoff <gwu20510@gmail.com>
2021-05-08 11:53:28 +0900

    Adapt carousel for responsive layout

--- src/components/page/sliderContainer.js
+++ src/components/page/sliderContainer.js
@@ -5,9 +5,9 @@ class SliderContainer extends React.Component {
         const { children, image_src } = this.props
     
         return (
-            <div className="section py-12 flex justify-center items-center">
-                <img className="w-1/2" src={ image_src } />
-                <div className="w-1/2">
+            <div className="section py-12 flex flex-wrap md:flex-row flex-col-reverse justify-center items-center">
+                <img className="w-full md:w-1/2 mt-6 md:mt-0" src={ image_src } />
+                <div className="w-full md:w-1/2">
                     { children }
                 </div>
             </div>
