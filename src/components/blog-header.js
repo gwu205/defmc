600e927f3ca2a4e5293e1969e2e0ee5ea0bd8144
Geoff <gwu20510@gmail.com>
2021-07-04 15:46:30 +0900

    Add link back to home

--- src/components/blog-header.js
+++ src/components/blog-header.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 import logo from "../../content/assets/defmc_logo_dark.svg"
 
 const BlogHeader = () => {
   return (
     <header class="w-auto">
        <div class="w-2/4 mx-auto py-8">
-          <img class="h-16 mb-0" src={logo} alt="DEFMC" />
+          <Link to={`/`}>
+            <img class="h-16 mb-0" src={logo} alt="DEFMC" />
+          </Link>
         </div>
     </header>
   )
