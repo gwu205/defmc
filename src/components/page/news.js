7553877b6629e067654eca51d738f47899ae3d32
Geoffrey Wu <geoffrey@crowdcast.jp>
2021-07-25 19:18:28 +0900

    Optimise news post listing on mobile

--- src/components/page/news.js
+++ src/components/page/news.js
@@ -8,38 +8,37 @@ class News extends React.Component {
     return (
       <section className="section py-16">
         <h2 className="heading-lg text-center mb-8">{t("news")}</h2>
-        <div className="max-w-2xl mx-auto border-4 border-primary rounded-lg p-6">
+        <dl className="max-w-2xl mx-auto border-4 border-primary rounded-lg p-6">
           {this.props.posts.map(({ node }) => {
             const title = node.frontmatter.title || node.fields.slug
             return (
-              <article className="flex" key={node.fields.slug}>
-                <p className="font-bold w-40 pr-4 flex-shrink-0">
+              <article
+                className="flex flex-wrap md:flex-nowrap"
+                key={node.fields.slug}
+              >
+                <p className="font-bold w-full text-xs md:text-base md:w-40 pr-4 flex-shrink-0">
                   {node.frontmatter.date}
                 </p>
                 <div>
-                  <header>
-                    <p className="font-bold">
-                      <Link
-                        className="shadow-none hover:text-primary hover:underline"
-                        to={node.fields.slug}
-                      >
-                        {title}
-                      </Link>
-                    </p>
-                  </header>
-                  <section>
-                    <p
-                      className="text-sm mt-2"
-                      dangerouslySetInnerHTML={{
-                        __html: node.frontmatter.description || node.excerpt,
-                      }}
-                    />
-                  </section>
+                  <dt className="font-bold">
+                    <Link
+                      className="shadow-none hover:text-primary hover:underline"
+                      to={node.fields.slug}
+                    >
+                      {title}
+                    </Link>
+                  </dt>
+                  <dd
+                    className="text-sm mt-2"
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
                 </div>
               </article>
             )
           })}
-        </div>
+        </dl>
       </section>
     )
   }
