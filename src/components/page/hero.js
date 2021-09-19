3e83bc8abc0a7c12655d456b5b4150811e11e30a
Geoffrey Wu <geoffrey@crowdcast.jp>
2021-09-19 23:48:16 +0900

    New design for hero section

--- src/components/page/hero.js
+++ src/components/page/hero.js
@@ -1,18 +1,21 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import { withTrans } from "../../i18n/withTrans"
 import Header from "../header"
 
 const Hero = ({ t, i18n }) => {
   return (
-    <section className="bg-hero">
+    <section className="banner">
       <Header />
-      <div className="flex max-w-screen-md mx-auto flex-wrap">
-        <div className="section py-12 md:py-24 w-full text-center">
-          <h1 className="font-sans font-bold text-4xl tracking-tight mb-5 md:text-7xl text-white">
+      <div className="flex min-h text-white md:text-primary">
+        <div className="flex flex-col justify-center px-4 md:pl-24 xl:pl-32 py-12 md:py-24 w-full md:w-1/2 text-center md:text-left">
+          <h1 className="font-sans font-bold text-6xl tracking-tight mb-6 md:text-6xl">
             {t("hero.tagline")}
           </h1>
-          <h2 className="font-sans text-2xl tracking-tight text-white my-4">
-            116 Blackburn Road, Doncaster East, VIC, 3109
+          <h2 className="font-sans font-bold text-2xl tracking-tight mb-12">
+            116 Blackburn Road,
+            <br />
+            Doncaster East, VIC, 3109
           </h2>
           <a
             href="https://www.hotdoc.com.au/medical-centres/doncaster-east-VIC-3109/doncaster-east-family-medical-centre/doctors"
@@ -34,17 +37,13 @@ const Hero = ({ t, i18n }) => {
             />
             <span className="ml-3 align-middle">{t("hero.button")}</span>
           </a>
-          <p className="font-sans text-white">{t("hero.CTA")}</p>
-          <h2 className="font-sans text-white">
+          <h2 className="font-sans text-xl">
             {t("hero.phone")}{" "}
             <a className="font-bold" href="tel:98428585">
               9842 8585
             </a>
           </h2>
         </div>
-        {/* <div className="w-full md:w-1/2 flex items-center justify-center block pt-12 md:pt-0 md:hidden">
-          <img className="h-96 mb-0" src={Doctor} />
-        </div> */}
       </div>
     </section>
   )
