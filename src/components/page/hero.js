278adf60a7ea3c546db4c4b84e6291457f12e6cd
Geoffrey Wu <geoffrey@crowdcast.jp>
2021-09-19 23:46:27 +0900

    Add hotdoc logo

--- src/components/page/hero.js
+++ src/components/page/hero.js
@@ -18,25 +18,20 @@ const Hero = ({ t, i18n }) => {
             href="https://www.hotdoc.com.au/medical-centres/doncaster-east-VIC-3109/doncaster-east-family-medical-centre/doctors"
             target="_blank"
             rel="noopener"
-            className="font-sans text-white border rounded p-4 my-4 shadow-none inline-block transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-25 hover:scale-105 hover:no-underline hover:opacity-100"
+            className="w-max 
+            flex items-center 
+            font-bold uppercase tracking-widest text-white md:text-green-700
+            border-2 border-white md:border-green-700 rounded 
+            p-4 mb-12 mx-auto md:mx-0 
+            shadow-none 
+            inline-block 
+            transition duration-300 ease-in-out transform hover:bg-white hover:bg-opacity-25 hover:scale-105 hover:no-underline hover:opacity-100"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="inline-block"
-            >
-              <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
-              <line x1="16" y1="2" x2="16" y2="6" />
-              <line x1="8" y1="2" x2="8" y2="6" />
-              <line x1="3" y1="10" x2="21" y2="10" />
-            </svg>
+            <StaticImage
+              src="../../../content/assets/hotdoc.png"
+              alt="Book appointment on HotDoc"
+              className="w-16"
+            />
             <span className="ml-3 align-middle">{t("hero.button")}</span>
           </a>
           <p className="font-sans text-white">{t("hero.CTA")}</p>
