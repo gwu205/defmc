c40f7035c2da2c1fbf9c2d8b50dfe34d5b293023
Geoffrey Wu <geoffrey@crowdcast.jp>
2021-09-26 14:01:24 +0900

    Update anchor tag

--- src/components/page/map.js
+++ src/components/page/map.js
@@ -35,7 +35,7 @@ const Map = ({ t }) => {
           <a
             href="https://www.facebook.com/defmc116"
             target="_blank"
-            rel="noopener"
+            rel="noreferrer"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
